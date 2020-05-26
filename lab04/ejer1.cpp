758075f5572edffd7f03eaf4e70d4ef617d28412
davidperlera <misaelramirez123a@gmail.com>
2020-05-26 12:07:48 -0600

    lab04

---
+++ lab04/ejer1.cpp
@@ -0,0 +1,29 @@
+#include "iostream"
+
+using namespace std;
+
+int main ()
+{
+
+    int num1,num2;
+
+    cout << "ingresar valor 1: ";
+    cin>>num1;
+
+    cout<<"ingresar valor 2: ";
+    cin>>num2;
+
+
+if(num1%num2==0)
+
+cout<<"si es divisible";
+
+else
+{
+
+    cout<<"no es divisible";
+}
+
+return 0;
+
+}
