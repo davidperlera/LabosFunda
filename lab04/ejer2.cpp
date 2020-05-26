80c06c1f638ac86de202ef3dca101ac8ce00a961
davidperlera <misaelramirez123a@gmail.com>
2020-05-26 12:08:36 -0600

    lab04

---
+++ lab04/ejer2.cpp
@@ -0,0 +1,25 @@
+#include "iostream"
+
+using namespace std;
+
+int main ()
+{
+
+    int numero;
+    cout<<endl;
+    cout<<"ingresar numero entero:";
+    cin>>numero;
+    cout<<endl;
+
+    if(numero%2==0)  
+    
+    cout<<"el numero es par";
+
+    else
+    {
+
+        cout<<"el numero es impar";
+    }
+
+return 0; 
+}
