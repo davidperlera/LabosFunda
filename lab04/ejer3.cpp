2ee4046a3feb8a21214d63c8647b96cb00a08d87
davidperlera <misaelramirez123a@gmail.com>
2020-05-26 12:09:06 -0600

    lab04

---
+++ lab04/ejer3.cpp
@@ -0,0 +1,39 @@
+#include "iostream"
+
+using namespace std;
+
+int main ()
+{
+
+    int numero;
+    cout<<endl;
+    cout<<"ingresar numero :";
+    cin>>numero;
+    cout<<endl;
+
+     if(numero==0)
+     {
+    cout<<"el numero es 0"<<endl;
+     }
+
+    if(numero>0)
+    {
+    cout<<"el numero es positivo"<<endl;
+
+    }
+     if(numero<0)
+    {
+    cout<<"el numero es negativo"<<endl;
+
+    }
+
+
+    return 0;
+   
+
+
+    
+
+
+
+}
